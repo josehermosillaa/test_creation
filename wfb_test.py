40f86ecc24ef562605643d1331b18a4c49eda785
josehermosillaa <jose.hermosillaa@usach.cl>
2025-09-26 13:04:14 -0300

    added wfb changes with R_BH_MIN

--- wfb_test.py
+++ wfb_test.py
@@ -5,6 +5,8 @@ from wf_groups import clasificar
 from test_br import is_br
 uses = ['R','CF', 'C','M']
 
+use_not_r = ['CF', 'C','M']
+
 columnas = ['R_BH_MIN', 'BR_R_BH_MAX', 'QAH_R_BH_MAX', 'BR_R_HEIGHT', 'QAH_R_HEIGHT']
 for use in uses:
     columnas.append(rf'{use}_BH_MIN')
@@ -14,6 +16,7 @@ for use in uses:
 @pytest.fixture(scope="module")
 def df():
     df = pd.read_csv("BR_REGULATION_C_SP.csv", sep=';', low_memory=False)
+    breakpoint()
     df = df[df['SA'] == 'WFB'].copy()
     df['ZD_WF'] = df['ZONEDIST'].apply(clasificar)
     df['permited_uses'] = df['ZONEDIST'].apply(permited_uses)
@@ -21,7 +24,6 @@ def df():
     df = df.replace({'true': True, 'false': False, 'True': True, 'False': False})
     # df = df.infer_objects(copy=False)
     
-    # breakpoint()
     return df
 
 def safe_convert(val):
@@ -32,4 +34,181 @@ def safe_convert(val):
     
 def test_ZD1_Height(df):
     df1 = df[df['ZD_WF'] == 'ZD1']
+    
+    for col in columnas:
+        df1[col] = df1[col].apply(safe_convert)
+    # breakpoint()
+    assert not df1.empty, "No hay datos para ZD1"
+    for idx, row in df1.iterrows():
+        if row['is_br'] == True:
+            if row['BR_R']==True:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 35)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 35)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+                    
+            for use in use_not_r:
+                if row[f'BR_{use}'] == True:
+                    if row['SB'] == True:
+                        assert pd.isna(row[f'{use}_BH_MIN'])
+                        assert (row[f'BR_{use}_BH_MAX'] == 35)
+                    else:
+                        assert (row[f'BR_{use}_HEIGHT'] == 35)
+        else:
+            if 'R' in row['permited_uses']:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 35)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 35)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+        
+            for item in row['permited_uses']:
+                if item == 'R':
+                    continue
+                if row['SB'] == True:
+                    assert pd.isna(row[f'{item}_BH_MIN'])
+                    assert (row[f'BR_{item}_BH_MAX'] == 35)
+                else:
+                    assert (row[f'BR_{item}_HEIGHT'] == 35), rf'Error en {item} fila {idx}'    
+        
+
+        # breakpoint()
+        
+def test_ZD2_Height(df):
+    df1 = df[df['ZD_WF'] == 'ZD2']
+
+    for col in columnas:
+        df1[col] = df1[col].apply(safe_convert)
+    # breakpoint()
+    assert not df1.empty, "No hay datos para ZD2"
+    for idx, row in df1.iterrows():
+        if row['is_br'] == True:
+            if row['BR_R']==True:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 35)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 45)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+                    
+            for use in use_not_r:
+                if row[f'BR_{use}'] == True:
+                    if row['SB'] == True:
+                        assert pd.isna(row[f'{use}_BH_MIN'])
+                        assert (row[f'BR_{use}_BH_MAX'] == 35)
+                    else:
+                        assert (row[f'BR_{use}_HEIGHT'] == 45)
+        else:
+            if 'R' in row['permited_uses']:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 35)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 45)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+        
+            for item in row['permited_uses']:
+                if item == 'R':
+                    continue
+                if row['SB'] == True:
+                    assert pd.isna(row[f'{item}_BH_MIN'])
+                    assert (row[f'BR_{item}_BH_MAX'] == 35)
+                else:
+                    assert (row[f'BR_{item}_HEIGHT'] == 45), rf'Error en {item} fila {idx}'
+        
+        
+def test_ZD3_Height(df):
+    df1 = df[df['ZD_WF'] == 'ZD3']
+
+    for col in columnas:
+        df1[col] = df1[col].apply(safe_convert)
+    # breakpoint()
+    assert not df1.empty, "No hay datos para ZD3"
+    for idx, row in df1.iterrows():
+        if row['is_br'] == True:
+            if row['BR_R']==True:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 45)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 55)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+                    
+            for use in use_not_r:
+                if row[f'BR_{use}'] == True:
+                    if row['SB'] == True:
+                        assert pd.isna(row[f'{use}_BH_MIN'])
+                        assert (row[f'BR_{use}_BH_MAX'] == 45)
+                    else:
+                        assert (row[f'BR_{use}_HEIGHT'] == 55)
+        else:
+            if 'R' in row['permited_uses']:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 45)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 55)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+        
+            for item in row['permited_uses']:
+                if item == 'R':
+                    continue
+                if row['SB'] == True:
+                    assert pd.isna(row[f'{item}_BH_MIN'])
+                    assert (row[f'BR_{item}_BH_MAX'] == 45)
+                else:
+                    assert (row[f'BR_{item}_HEIGHT'] == 55), rf'Error en {item} fila {idx}'
+
+
+def test_ZD4_Height(df):
+    df1 = df[df['ZD_WF'] == 'ZD4']
+
+    for col in columnas:
+        df1[col] = df1[col].apply(safe_convert)
     breakpoint()
+    assert not df1.empty, "No hay datos para ZD4"
+    for idx, row in df1.iterrows():
+        if row['is_br'] == True:
+            if row['BR_R']==True:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 55)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 65)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+                    
+            for use in use_not_r:
+                if row[f'BR_{use}'] == True:
+                    if row['SB'] == True:
+                        assert pd.isna(row[f'{use}_BH_MIN'])
+                        assert (row[f'BR_{use}_BH_MAX'] == 55)
+                    else:
+                        assert (row[f'BR_{use}_HEIGHT'] == 65)
+        else:
+            if 'R' in row['permited_uses']:
+                if row['SB'] == True:
+                    assert pd.isna(row['R_BH_MIN'])
+                    assert (row['BR_R_BH_MAX'] == 55)
+                    assert pd.isna(row['QAH_R_BH_MAX'])
+                else:
+                    assert (row['BR_R_HEIGHT'] == 65)
+                    assert pd.isna(row['QAH_R_HEIGHT'])
+        
+            for item in row['permited_uses']:
+                if item == 'R':
+                    continue
+                if row['SB'] == True:
+                    assert pd.isna(row[f'{item}_BH_MIN'])
+                    assert (row[f'BR_{item}_BH_MAX'] == 55)
+                else:
+                    assert (row[f'BR_{item}_HEIGHT'] == 65), rf'Error en {item} fila {idx}'
